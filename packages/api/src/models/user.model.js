99cf1635fe05964cd693034ad36de688d0f56f95
jjenkins88 <jjenkinspdxcode@gmail.com>
2023-05-29 19:12:12 -0700

    Initial User Model

---
+++ packages/api/src/models/user.model.js
@@ -0,0 +1,52 @@
+import bcrypt from 'bcrypt';
+
+module.exports = (sequelize, Sequelize) => {
+    const User = sequelize.define('User', {
+        id: {
+            type: Sequelize.UUID,
+            defaultValue: UUIDV4,
+            primaryKey: true,
+            allowNull: false
+                },
+        username : {
+            type: Sequelize.STRING,
+            allowNull: false,
+            indexes: [
+                {
+                    fields: ['username']
+                }
+            ]
+        },
+        email: {
+            type: Sequelize.STRING,
+            allowNull: false,
+            indexes: [
+                {
+                    fields: ['email']
+                }
+            ]
+        },
+        password: {
+            type: Sequelize.STRING,
+            allowNull: false
+        },
+        hooks: {
+            beforeCreate: (user) => {
+                if (user.password) {
+                    const salt = bcrypt.genSaltSync(10, 'a');
+                    user.password = bcrypt.hashSync(user.password, salt);
+                }
+            }
+        }
+        
+    });
+
+    User.prototype.validPassword = function(password, hash) {
+        return bcrypt.compareSync(password, hash);
+    }
+
+};
+
+return User;
+
+
