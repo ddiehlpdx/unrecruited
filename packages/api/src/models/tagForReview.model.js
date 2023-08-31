550f3367465c5145f9aefa3980483191f5046c48
jjenkins88 <jjenkinspdxcode@gmail.com>
2023-08-31 16:29:13 -0700

    Changed to ES6

--- packages/api/src/models/tagForReview.model.js
+++ packages/api/src/models/tagForReview.model.js
@@ -1,33 +1,36 @@
-export default(sequelize, Sequelize) => {
-    const tagForReview = sequelize.define('tagForReview', {
-        id: {
-            type: Sequelize.UUID,
-            defaultValue: UUIDV4,
-            primaryKey: true,
-            allowNull: false
-        },
-        reviewId: {
-            type: Sequelize.UUID,
-            defaultValue: UUIDV4,
-            allowNull: false,
-            indexes: [
-                {
-                    fields: ['reviewId']
-                }
-            ]
-        },
-        tagId: {
-            type: Sequelize.UUID,
-            defaultValue: UUIDV4,
-            allowNull: false,
-            indexes: [
-                {
-                    fields: ['tagId']
-                }
-            ]
-        }
-        
-    });
-    return tagForReview
-};
+import { Sequelize, DataTypes } from 'sequelize';
+
+const sequelize = new Sequelize('sqlite::memory:');
+
+const tagForReview = sequelize.define('tagForReview', {
+    id: {
+        type: DataTypes.UUID,
+        defaultValue: UUIDV4,
+        primaryKey: true,
+        allowNull: false
+    },
+    reviewId: {
+        type: DataTypes.UUID,
+        defaultValue: UUIDV4,
+        allowNull: false,
+        indexes: [
+            {
+                fields: ['reviewId']
+            }
+        ]
+    },
+    tagId: {
+        type: DataTypes.UUID,
+        defaultValue: UUIDV4,
+        allowNull: false,
+        indexes: [
+            {
+                fields: ['tagId']
+            }
+        ]
+    }
+});
+
+export default tagForReview;
+
 
