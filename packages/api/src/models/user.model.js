23c842f69d52ab8352dc484bd3f7c85179d94165
jjenkins88 <jjenkinspdxcode@gmail.com>
2023-08-31 16:18:35 -0700

    Changed to ES6 Model

--- packages/api/src/models/user.model.js
+++ packages/api/src/models/user.model.js
@@ -1,53 +1,56 @@
+import { Sequelize, DataTypes } from 'sequelize';
 import bcrypt from 'bcrypt';
 
-export default(sequelize, Sequelize) => {
-    const User = sequelize.define('User', {
-        id: {
-            type: Sequelize.UUID,
-            defaultValue: UUIDV4,
-            primaryKey: true,
-            allowNull: false
-                },
-        username : {
-            type: Sequelize.STRING,
-            allowNull: false,
-            indexes: [
-                {
-                    fields: ['username']
-                }
-            ]
-        },
-        email: {
-            type: Sequelize.STRING,
-            allowNull: false,
-            indexes: [
-                {
-                    fields: ['email']
-                }
-            ]
-        },
-        password: {
-            type: Sequelize.STRING,
-            allowNull: false
-        },
-        hooks: {
-            beforeCreate: (user) => {
-                if (user.password) {
-                    const salt = bcrypt.genSaltSync(10, 'a');
-                    user.password = bcrypt.hashSync(user.password, salt);
-                }
+const sequelize = new Sequelize('sqlite::memory:');
+
+const User = sequelize.define('User', {
+    id: {
+        type: DataTypes.UUID,
+        defaultValue: UUIDV4,
+        primaryKey: true,
+        allowNull: false
+    },
+    username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        indexes: [
+            {
+                fields: ['username']
+            }
+            
+        ]
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        indexes: [
+            {
+                fields: ['email']
+            }
+        ]
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    hooks: {
+        beforeCreate: (user) => {
+            if (user.password) {
+                const salt = bcrypt.genSaltSync(10, 'a');
+                user.password = bcrypt.hashSync(user.password, salt);
             }
         }
+    }
+});  
         
-    });
+ 
 
     User.prototype.validPassword = function(password, hash) {
         return bcrypt.compareSync(password, hash);
     }
+    
 
-    return User;
-};
-
+export default User
 
 
 
